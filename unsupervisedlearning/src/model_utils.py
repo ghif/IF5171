249048be40d81a9c7b03db8272fb7029666af0b0
mghifary <muhammad.ghifary@wartek.belajar.id>
2022-12-03 20:25:17 +0700

    vae

--- unsupervisedlearning/src/model_utils.py
+++ unsupervisedlearning/src/model_utils.py
@@ -36,6 +36,27 @@ class MLPAutoEnc(nn.Module):
         self.encoder = nn.Sequential(
             nn.Linear(d_in, d_h),
             nn.ReLU(),
+            nn.Linear(d_h, d_z)
+        )
+        
+        self.decoder = nn.Sequential(
+            nn.Linear(d_z, d_h),
+            nn.ReLU(),
+            nn.Linear(d_h, d_in),
+            nn.Sigmoid()
+        )
+        
+    def forward(self, x):
+        z = self.encoder(x)
+        xr = self.decoder(z)
+        return xr
+    
+class AutoEncSigm(nn.Module):
+    def __init__(self, d_in=28*28, d_z=100, d_h=512):
+        super(AutoEncSigm, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(d_in, d_h),
+            nn.ReLU(),
             nn.Linear(d_h, d_z),
             nn.Sigmoid()
         )
@@ -52,6 +73,71 @@ class MLPAutoEnc(nn.Module):
         xr = self.decoder(z)
         return xr
     
+class AutoEncFMNIST(nn.Module):
+    def __init__(self, d_in=28*28, d_z=100, d_h=512):
+        super(AutoEncFMNIST, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(d_in, d_h),
+            nn.ReLU(),
+            nn.Linear(d_h, d_z),
+            nn.Sigmoid()
+        )
+        
+        self.decoder = nn.Sequential(
+            nn.Linear(d_z, d_h),
+            nn.ReLU(),
+            nn.Linear(d_h, d_in),
+            nn.Sigmoid()
+        )
+        
+    def forward(self, x):
+        z = self.encoder(x)
+        xr = self.decoder(z)
+        return xr
+    
+class VAE(nn.Module):
+    def __init__(self, d_in=28*28, d_z=100, d_h=512):
+        super(VAE, self).__init__()
+        
+        self.d_z = d_z
+        
+        self.encoder = nn.Sequential(
+            nn.Linear(d_in, d_h),
+            nn.ReLU(),
+            nn.Linear(d_h, d_z * 2),
+        )
+        
+        self.decoder = nn.Sequential(
+            nn.Linear(d_z, d_h),
+            nn.ReLU(),
+            nn.Linear(d_h, d_in),
+            nn.Sigmoid()
+        )
+        
+    def reparameterise(self, mu, logvar):
+        if self.training:
+            std = logvar.mul(0.5).exp_()
+            eps = std.new_empty(std.size()).normal_()
+            return eps.mul_(std).add_(mu)
+        else:
+            return mu
+    
+    def encode(self, x):
+        mu_logvar = self.encoder(x)
+        mu_logvar = mu_logvar.reshape(-1, 2, self.d_z)
+        mu = mu_logvar[:, 0, :]
+        logvar = mu_logvar[:, 1, :]
+        z = self.reparameterise(mu, logvar)
+        return z, mu, logvar
+    
+    def decode(self, z):
+        return self.decoder(z)
+        
+    def forward(self, x):
+        z, mu, logvar = self.encode(x)
+        xr = self.decode(z)
+        return xr, mu, logvar
+        
 class ConvNet(nn.Module):
     def __init__(self, d_out=10):
         super(ConvNet, self).__init__()
